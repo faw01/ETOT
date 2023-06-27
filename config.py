5d30e8ae5e283d299a5b68377f82d5e47aabfaa6
faw <fawwad30@gmail.com>
2023-06-28 00:27:23 +0800

    introduced oop principles
    - remove typos
    - restructured file

--- config.py
+++ config.py
@@ -1,3 +1,2 @@
-# config.py
 API_KEY = 'your_api_key_here'
 DATABASE_URI = 'your_database_uri_here'
--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-# main.py
-from api import get_food_data as get_food_data_from_api
-from database import save_food_data, get_food_data, compare_foods
+from api import *
+from database import *
 
 def main():
-    # Get user input for the foods to compare
-    # Check if the data is already in the database
-    # If not, fetch it from the API and save it
-    # Compare the foods and print the comparison
     pass
-
-def weight_loss():
-    # Gets called in main() if the user chooses the "Weight Loss" option. 
-    # Priority of nutrients to be confirmed
-    pass
-
-def build_muscle():
-    pass
-
-
 
 if __name__ == '__main__':
     main()
